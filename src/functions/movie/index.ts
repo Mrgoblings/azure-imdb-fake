b8d1d2d047efd59df96aba619bf902063a28dd40
MrGoblings <emomomchev@gmail.com>
2024-04-18 10:04:28 +0300

    add: functionallity for movie and thumbnails

---
+++ src/functions/movie/index.ts
@@ -0,0 +1,75 @@
+import { app, HttpRequest, HttpResponseInit, InvocationContext } from "@azure/functions";
+import { Prisma, PrismaClient } from "@prisma/client";
+import * as Joi from 'joi';
+
+const prisma = new PrismaClient();
+
+export async function movie(request: HttpRequest, context: InvocationContext): Promise<HttpResponseInit> {
+    context.log(`Http function processed request for url "${request.url}"`);
+
+    switch (request.method) {
+        case "GET":
+            return getMovies();
+        case "POST":
+            return createMovie(request);
+        case "PUT":
+            return updateMovie(request);
+        case "DELETE":
+            return deleteMovie(request);
+        default:
+            return { status: 405, body: "Method Not Allowed" };
+    }
+}
+
+async function getMovies(): Promise<HttpResponseInit> {
+    try {
+        const movies = await prisma.movie.findMany();
+        return { body: JSON.stringify(movies) };
+    } catch (error) {
+        return { status: 500, body: "Internal Server Error" };
+    }
+}
+async function createMovie(request: HttpRequest): Promise<HttpResponseInit> {
+    try {
+        const movieData: Prisma.MovieUncheckedCreateInput = await Joi.object({
+            title: Joi.string().required(),
+            year: Joi.number().integer().required(),
+            genre: Joi.string().required(),
+            description: Joi.string().required(),
+            // Add validation for other properties
+        }).validateAsync(request.body);
+
+        const movie = await prisma.movie.create( {data: movieData} );
+        return { body: JSON.stringify(movie) };
+    } catch (error) {
+        return { status: 500, body: "Internal Server Error" };
+    }
+}
+
+async function updateMovie(request: HttpRequest): Promise<HttpResponseInit> {
+    try {
+        const movieId = parseInt(request.query.get("id") || "");
+        const movieData = request.body;
+        const movie = await prisma.movie.update({ where: { id: movieId }, data: movieData });
+        return { body: JSON.stringify(movie) };
+    } catch (error) {
+        return { status: 500, body: "Internal Server Error" };
+    }
+}
+
+async function deleteMovie(request: HttpRequest): Promise<HttpResponseInit> {
+    try {
+        const movieId = parseInt(request.query.get("id") || "");
+        await prisma.movie.delete({ where: { id: movieId } });
+        return { status: 204 };
+    } catch (error) {
+        return { status: 500, body: "Internal Server Error" };
+    }
+}
+
+app.http('movie', {
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    authLevel: 'anonymous',
+    handler: movie
+});
+
---
+++ src/functions/movie/thumbnail/index.ts
@@ -0,0 +1,55 @@
+import { app, HttpRequest, HttpResponseInit, InvocationContext } from "@azure/functions";
+import { Prisma, PrismaClient } from "@prisma/client";
+import * as Joi from 'joi';
+
+const prisma = new PrismaClient();
+
+export async function thumbnail(request: HttpRequest, context: InvocationContext): Promise<HttpResponseInit> {
+    switch (request.method) {
+        case 'GET':
+            return await getThumbnail(request);
+        case 'PUT':
+            return await updateThumbnail(request);
+        case 'DELETE':
+            return await deleteThumbnail(request);
+        default:
+            return { status: 405, body: 'Method Not Allowed' };
+    }
+}
+
+async function getThumbnail(request: HttpRequest): Promise<HttpResponseInit> {
+    try {
+        const movieId = parseInt(request.query.get("id") || "");
+        const movie = await prisma.movie.findUnique({ where: { id: movieId }, select: { thumbnailUrl: true } });
+        return { body: JSON.stringify(movie?.thumbnailUrl) };
+    } catch (error) {
+        return { status: 500, body: "Internal Server Error" };
+    }
+}
+
+async function updateThumbnail(request: HttpRequest): Promise<HttpResponseInit> {
+    try {
+        const movieId = parseInt(request.query.get("id") || "");
+        const thumbnailUrl: string = await Joi.string().uri().validateAsync(request.body);
+        const movie = await prisma.movie.update({ where: { id: movieId }, data: { thumbnailUrl } });
+        return { body: JSON.stringify(movie.thumbnailUrl) };
+    } catch (error) {
+        return { status: 500, body: "Internal Server Error" };
+    }
+}
+
+async function deleteThumbnail(request: HttpRequest): Promise<HttpResponseInit> {
+    try {
+        const movieId = parseInt(request.query.get("id") || "");
+        const movie = await prisma.movie.update({ where: { id: movieId }, data: { thumbnailUrl: null } });
+        return { status: 204 };
+    } catch (error) {
+        return { status: 500, body: "Internal Server Error" };
+    }
+}
+
+app.http('thumbnail', {
+    methods: ['GET', 'PUT', 'DELETE'],
+    authLevel: 'anonymous',
+    handler: thumbnail
+});
